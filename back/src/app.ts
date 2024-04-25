8b470f1c097a6135ca179ea83ef2bf3457c82aa0
Warheart-1 <axel97477@gmail.com>
2024-04-25 12:12:08 +0200

    [ADD] connection to mongodb database

---
+++ back/src/app.ts
@@ -0,0 +1,31 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+const uri = process.env.MONGODB_URI || '';
+mongoose.connect(uri);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.use(cors());
+app.use(express.json());
+
+//app.use('/api');
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
