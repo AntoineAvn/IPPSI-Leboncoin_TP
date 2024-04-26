014b675762ae7a276acf66e9938dd07a6cb6c4a1
Warheart-1 <axel97477@gmail.com>
2024-04-26 11:26:41 +0200

    [UPDATE] announces remove in cascade

--- back/src/models/users/user.schema.ts
+++ back/src/models/users/user.schema.ts
@@ -11,6 +11,25 @@ const userSchema = new Schema({
     isAdmin: { type: Boolean, default: false, require: true }
 })
 
+// Pour delete en cascade
+userSchema.pre('findOneAndUpdate', async function(next) {
+    const update : any = this.getUpdate();
+    const annonceIdToDelete = update?.$pull?.annonces;
+
+    if (annonceIdToDelete) {
+        try {
+            await this.model.updateMany(
+                { annonces: annonceIdToDelete },
+                { $pull: { annonces: annonceIdToDelete } } 
+            );
+        } catch (error) {
+            console.error(`Error deleting annonce reference from users: ${error}`);
+        }
+    }
+
+    next();
+});
+
 const User = model<IUser>('User', userSchema)
 
 export default User
